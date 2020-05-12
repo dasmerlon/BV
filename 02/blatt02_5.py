06856c2c1b08a1fc682502a6b2b2a8cfc2d31b8c
Merle Hoffmann <hoffmannmerle@web.de>
2020-05-12 22:42:45 +0200

    scaling noised image

--- 02/blatt02_5.py
+++ 02/blatt02_5.py
@@ -16,25 +16,38 @@ from skimage.io import imread
 
 plt.close('all')
 mandrill = imread('./mandrill.png')
+
 fig1 = plt.figure(1)
-fig1.suptitle('kein Rauschen')
+fig1.suptitle('without noise')
 plt.imshow(mandrill, cmap = 'gray')
+
+
+def scaling_image(img):
+    img_m = img - np.min(img)
+    img_s = 255 * (img_m / np.max(img_m))
+    return img_s
 
 
 def gaussian_noise(img, sigma):
     noise = np.random.normal(0, sigma, img.shape)
     img_n = img + noise
-    plt.imshow(img_n, cmap = 'gray')
+    img_s = scaling_image(img_n)
+    plt.imshow(img_s, cmap = 'gray')
 
-
+#the smaller the SNR, the larger the noise ratio.
+    
 #def salt_and_pepper_noise(img, p):
+#    noise = salt + pepper
 #    img_n = img + #salt and pepper noise
 #    plt.imshow(img_n, cmap = 'gray')
 
 
 fig2 = plt.figure(2)
-fig2.suptitle('Gaußsches Rauschen')
+fig2.suptitle('gaussian noise')
 gaussian_noise(mandrill, 20)
 
+fig3 = plt.figure(3)
+fig3.suptitle('salt-and-pepper noise')
+#salt_and_pepper_noise(mandrill, 0.5)
 
 plt.show()
