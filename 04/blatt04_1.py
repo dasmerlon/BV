41f1fbfce1427ee5cad19d10cf97daa71a84bbf1
Mezix <mezteenx@googlemail.com>
2020-05-26 17:51:58 +0200

    Solution for exercise 1

--- 04/blatt04_1.py
+++ 04/blatt04_1.py
@@ -39,25 +39,19 @@ plt.close('all')
    unteren linken Bildecke gibt?
 """
 
+"""
+Gelöst mit einer Power Law Transformation. Es gibt 8 Poller.
+"""
+
 beforeAImage = img1
-afterAImage = img1
-
-#Img1Min = np.min(beforeAImage)
-#print(Img1Min)
-
-#Img1Max = np.max(beforeAImage)
-#print(Img1Max)
-
-"""
-gamma correction?
-"""
+afterAImage = img1 ** 1.55
 
 beforeA = plt.figure(1)
-beforeA.suptitle('Before')
+beforeA.suptitle('Image 1 before gamma correction')
 plt.imshow(beforeAImage, cmap = 'gray', vmin=0, vmax=255)
 
 afterA = plt.figure(2)
-afterA.suptitle("After")
+afterA.suptitle("Image 1 after gamma correction")
 plt.imshow(afterAImage, cmap = 'gray', vmin=0, vmax=255)
 
 
@@ -65,10 +59,40 @@ plt.imshow(afterAImage, cmap = 'gray', vmin=0, vmax=255)
 2. Sorgt dafür, dass sich die Skyline in Bild 2 besser vom Himmel abhebt.
 """
 
+"""
+Contrast Stretching/Intensity Slicing
+"""
 
-"""
-power law transformation?
-"""
+def ContrastStretching(image):
+    
+    result = np.copy(image)
+    for x in range(result.shape[0]):
+        for y in range(result.shape[1]):
+            if(result[x,y] < 145):
+                result[x,y] = result[x,y] ** 0.8
+            elif(result[x,y] > 150):
+                result[x,y] = result[x,y] ** 1.05
+    return result
+
+beforeBImage = img2
+afterBImageContrastStretching = ContrastStretching(img2)
+
+schwellWert = (beforeBImage > 147) * 255
+
+afterBImage = beforeBImage + schwellWert
+
+beforeB = plt.figure(3)
+beforeB.suptitle('Image 2 Before')
+plt.imshow(beforeBImage, cmap = 'gray', vmin=0, vmax=255)
+
+afterB = plt.figure(4)
+afterB.suptitle("Image 2 with Intensity Level Slicing")
+plt.imshow(afterBImage, cmap = 'gray', vmin=0, vmax=255)
+
+afterB = plt.figure(5)
+afterB.suptitle("Image 2 with Contrast Stretching")
+plt.imshow(afterBImageContrastStretching, cmap = 'gray', vmin=0, vmax=255)
+
 
 """
 3. Verändert das Bild 3 so, dass in etwa der Bereich der Autobahn in der 
@@ -77,9 +101,22 @@ power law transformation?
 """
 
 """
-Intensity Level Slicing!
+Gelöst mit Intensity Level Slicing!
 """
 
+beforeCImage = img3
+
+schwellWert = (beforeCImage > 104) * 255
+
+afterCImage = beforeCImage + schwellWert
+
+beforeC = plt.figure(6)
+beforeC.suptitle('Image 3 before Intensity Slicing')
+plt.imshow(beforeCImage, cmap = 'gray', vmin=0, vmax=255)
+
+afterC = plt.figure(7)
+afterC.suptitle("Image 3 after Intensity Slicing")
+plt.imshow(afterCImage, cmap = 'gray', vmin=0, vmax=255)
 
 # für den Grauwertbereich 0 bis 1
 #plt.imshow(result, cmap='gray', vmin=0, vmax=1)
