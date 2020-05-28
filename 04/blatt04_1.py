de14cac350eaa2862b45e78489e094dc694a6d7c
Merle Hoffmann <hoffmannmerle@web.de>
2020-05-28 22:43:49 +0200

    minor changes

--- 04/blatt04_1.py
+++ 04/blatt04_1.py
@@ -23,7 +23,6 @@ niedrigen Grauwerte hat.
 
 import numpy as np
 import matplotlib.pyplot as plt
-import math as math
 from skimage.io import imread
 
 
@@ -38,61 +37,48 @@ plt.close('all')
    und ermittelt, wie viele Poller es am Rande einer kleinen Grüninsel in der 
    unteren linken Bildecke gibt?
 """
-
-"""
-Gelöst mit einer Power Law Transformation. Es gibt 8 Poller.
-"""
-
+# Gelöst mit einer Power Law Transformation. Es gibt 8 Poller.
 beforeAImage = img1
-afterAImage = img1 ** 1.55 #um den contrast zu erhöhen von dem dunklem bereich, benutzten wir eine exponentialfunktion und 
-                           #rechnen jeden pixel hoch einen wert der größer als 1 ist
+afterAImage = img1 ** 1.55 
+# Um den Kontrast von dem dunklem Bereich zu erhöhen, benutzten wir eine 
+# Exponentialfunktion und rechnen jeden Pixel hoch einen Wert größer als 1.
 
-#beforeA = plt.figure(1)
-#beforeA.suptitle('Image 1 before gamma correction')
-#plt.imshow(beforeAImage, cmap = 'gray', vmin=0, vmax=255)
-
-afterA = plt.figure(2)
-afterA.suptitle("Image 1 after gamma correction")
-plt.imshow(afterAImage, cmap = 'gray', vmin=0, vmax=255)
+f, f1 = plt.subplots(1,2) 
+f.suptitle('Image 1 before and after gamma correction')
+f1[0].imshow(beforeAImage, cmap = 'gray', vmin=0, vmax=255)
+f1[1].imshow(afterAImage, cmap = 'gray', vmin=0, vmax=255)
 
 
 """
 2. Sorgt dafür, dass sich die Skyline in Bild 2 besser vom Himmel abhebt.
 """
-
-"""
-Contrast Stretching/Intensity Slicing
-"""
-
-def ContrastStretching(image): #eine funktion die auf einer pixel per pixel basis jeden wert auf seinen grauwert untersucht und dann entsprechend, contrast erhöht oder verringert
-    
+# Contrast Stretching
+def ContrastStretching(image): 
+    # Eine Funktion, die auf einer Pixel-per-Pixel-Basis jeden Wert auf seinen 
+    # Grauwert untersucht und dann entsprechend, den Kontrast erhöht oder
+    # verringert.
     result = np.copy(image)
     for x in range(result.shape[0]):
         for y in range(result.shape[1]):
-            if(result[x,y] < 145): #unsere skyline liegt ca zwischen 145 und 150
-                result[x,y] = result[x,y] ** 0.8 #hellere werte sollen heller sein
+            if(result[x,y] < 145): # Die Skyline liegt ca. zwischen 145 und 150
+                result[x,y] = result[x,y] ** 0.8 #hellere Eerte --> heller
             elif(result[x,y] > 150):
-                result[x,y] = result[x,y] ** 1.05 #dunklere werte sollen dunkler werden
+                result[x,y] = result[x,y] ** 1.05 #dunklere werte --> dunkler
     return result
 
 beforeBImage = img2
 afterBImageContrastStretching = ContrastStretching(img2)
-
-schwellWert = (beforeBImage > 147) * 255 #hier einmal noch ein versuch mit intensity level slicing
-
+# Hier einmal noch ein Versuch mit Intensity Level Slicing.
+schwellWert = (beforeBImage > 147) * 255 
 afterBImage = beforeBImage + schwellWert
 
-#beforeB = plt.figure(3)
+#beforeB = plt.figure(2)
 #beforeB.suptitle('Image 2 Before')
 #plt.imshow(beforeBImage, cmap = 'gray', vmin=0, vmax=255)
-
-afterB = plt.figure(4)
-afterB.suptitle("Image 2 with Intensity Level Slicing")
-plt.imshow(afterBImage, cmap = 'gray', vmin=0, vmax=255)
-
-afterB = plt.figure(5)
-afterB.suptitle("Image 2 with Contrast Stretching")
-plt.imshow(afterBImageContrastStretching, cmap = 'gray', vmin=0, vmax=255)
+f, f2 = plt.subplots(1,2) 
+f.suptitle('Image 2 with Intensity Level Slicing and Contrast Stretching')
+f2[0].imshow(afterBImage, cmap = 'gray', vmin=0, vmax=255)
+f2[1].imshow(afterBImageContrastStretching, cmap = 'gray', vmin=0, vmax=255)
 
 
 """
@@ -100,27 +86,18 @@ plt.imshow(afterBImageContrastStretching, cmap = 'gray', vmin=0, vmax=255)
    Bildmitte weiß ist und der Rest des Bildes seine Graufärbung behält.
    Hinweis: Das Ergebnis wird nicht perfekt werden!
 """
-
-"""
-Gelöst mit Intensity Level Slicing!
-"""
-
+# Gelöst mit Intensity Level Slicing.
 beforeCImage = img3
 
-schwellWert = (beforeCImage > 104) * 255 #wir machen eine maske von allen pixel über dem grauwert 104 und machen die matrix vollständig weiß
+schwellWert = (beforeCImage > 104) * 255 
+# Wir erzeugen eine Maske und färben alle Pixel über dem Grauwert 104 weiß.
+afterCImage = beforeCImage + schwellWert 
+# Indem wir die Maske mit dem Bild addieren sorgen wir dafür das alle richtigen 
+# Pixel automatisch den wert 255 im fertigen Bild haben.
 
-afterCImage = beforeCImage + schwellWert #indem wir die maske addieren sorgen wir dafür das alle richtigen pixel automatisch den wert 255
-                                         #im fertigen bild haben
+f, f3 = plt.subplots(1,2) 
+f.suptitle('Image 3 before and after Intensity Slicing')
+f3[0].imshow(beforeCImage, cmap = 'gray', vmin=0, vmax=255)
+f3[1].imshow(afterCImage, cmap = 'gray', vmin=0, vmax=255)
 
-#beforeC = plt.figure(6)
-#beforeC.suptitle('Image 3 before Intensity Slicing')
-#plt.imshow(beforeCImage, cmap = 'gray', vmin=0, vmax=255)
-
-afterC = plt.figure(7)
-afterC.suptitle("Image 3 after Intensity Slicing")
-plt.imshow(afterCImage, cmap = 'gray', vmin=0, vmax=255)
-
-# für den Grauwertbereich 0 bis 1
-#plt.imshow(result, cmap='gray', vmin=0, vmax=1)
-# oder für den Grauwertbereich 0 bis 255
-#plt.imshow(result, cmap='gray', vmin=0, vmax=255)
+plt.show()
